570b1b3a0fcc3456708f1f87c2b100440880ae01
David Rauschuber <djr715@gmail.com>
2026-07-10 04:30:15 -0500

    feat: add cli args for more ffmpeg params

--- src/convert_video/main.py
+++ src/convert_video/main.py
@@ -1,11 +1,15 @@
 import argparse
+import datetime
+import sys
+import time
 from argparse import ArgumentParser
 from ast import arg
 from pathlib import Path
 
 import ffmpeg
 
-from .convert import run
+from .convert import format_run_time_str, run
+from .logger import logger
 
 
 def validate_path(path):
@@ -45,12 +49,28 @@ def parse_arguments():
     parser = ArgumentParser(description="Convert videos to a smaller size")
 
     parser.add_argument("path", type=validate_path, help="can be a directory or file")
+
     parser.add_argument(
         "-r",
         "--recursive",
         action="store_true",
         help="set to recursively search directories for videos to convert",
     )
+
+    parser.add_argument(
+        "-s",
+        "--suffix",
+        action="store",
+        default="converted",
+        help="suffix added to file name after converting: vid1.mp4 -> vid1.converted.mp4",
+    )
+
+    parser.add_argument(
+        "--overwrite-existing",
+        action="store_true",
+        help="set to overwrite output file if it exists",
+    )
+
     parser.add_argument(
         "-sf",
         "--scale",
@@ -84,6 +104,7 @@ def parse_arguments():
         default="slow",
         help="Select ffmpeg x265 preset",
     )
+
     parser.add_argument(
         "-t",
         "--tune",
@@ -93,18 +114,36 @@ def parse_arguments():
         help="Select ffmpeg x265 tune",
     )
 
-    parser.add_argument("-g", "--gop", action="store", type=int, help="GOP Interval")
+    parser.add_argument(
+        "--pix_fmt",
+        action="store",
+        default="yuv444p10le",
+        help="pixel format for ffmpeg",
+    )
+
+    parser.add_argument(
+        "-g",
+        "--gop_interval",
+        action="store",
+        type=int,
+        help="GOP Interval. Used to calculate values for g and keyint_min",
+    )
+
     args = parser.parse_args()
     return {k: v for k, v in vars(args).items() if v is not None}
 
 
 def main():
+    start_time = datetime.datetime.fromtimestamp(time.time())
     args = parse_arguments()
     print(args)
     path = args.pop("path")
     recursive = args.pop("recursive", False)
 
     run(path, recursive=recursive, **args)
+    finish_time = datetime.datetime.fromtimestamp(time.time())
+    logger.info(f"finished")
+    logger.info(f"Total runtime: {format_run_time_str(start_time,finish_time)}")
 
 
 if __name__ == "__main__":
